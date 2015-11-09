357c33b5b3afe2c330e41eadac3eb2f18c50f42e
clouis-charles <christine.louischarl@f1006-26-34158.bmcc.cuny.edu>
2015-11-09 15:11:00 -0500

    made pig head and eyes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
-function setup(){
-    createCanvas(windowWidth, windowHeight); 
+function setup() {
+    createCanvas(windowWidth,windowHeight); 
     
-
-
-
-} 
-
-
-function mousePressed() {
-
-
-
+drawPig();
 
 
 } 
+
+
 
 function drawPig() {
 
+// left ear 
+    
+    
+    
+    
+    
+//right ear 
+    
+    
+    
+    
+    
+    
+// body 
+    
+    ellipse(windowWidth/2, windowHeight/2, 500, 450);
 
-
-
-
-
-
-
-
+// eyes 
+    ellipse(windowWidth/2-75, windowHeight/2-50, 50, 50); 
+    ellipse(windowWidth/2+75, windowHeight/2-50, 50, 50); 
+   
+    
+// nose 
+    
+    
 
 
 
