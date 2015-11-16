dd5308d47261014e7f01f34efcfc39557cdad027
clouis-charles <christine.louischarl@f1006-26-34158.bmcc.cuny.edu>
2015-11-16 16:53:29 -0500

    added new function
    discarded star idea, make new pattern with triangles. make the
    triangles change color when the pig is clicked

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function setup() {
     createCanvas(windowWidth,windowHeight); 
     
-drawPig();
-
-
+  
+    //drawPig();
+    drawPattern();
 } 
 
 
 
-function drawPig() {
+/*function drawPig() {
 
 // left ear 
     fill(253, 215, 228);
@@ -16,14 +16,10 @@ function drawPig() {
 //  bezier(85, 40, 80, 10, 10, 10, 15, 60);
     bezier(windowWidth/2-190, windowHeight/2-10, windowWidth/2-400, windowHeight/2-100, windowWidth/2-300, windowHeight/2-300, windowWidth/2-150, windowHeight/2-180);
     
-    
-    
 //right ear 
     bezier(windowWidth/2+190, windowHeight/2-10, windowWidth/2+400, windowHeight/2-100, windowWidth/2+300, windowHeight/2-300, windowWidth/2+150, windowHeight/2-180);
     
-    
-    
-// body 
+// head 
     
     ellipse(windowWidth/2, windowHeight/2, 500, 450);
 
@@ -32,14 +28,22 @@ function drawPig() {
     ellipse(windowWidth/2-90, windowHeight/2-50, 50, 50); 
     ellipse(windowWidth/2+90, windowHeight/2-50, 50, 50); 
    
-    
 // nose 
     fill(196, 129, 137);
-    ellipse(windowWidth/2, windowHeight/2+60, 200, 150) 
+    ellipse(windowWidth/2, windowHeight/2+60, 200, 150); 
     fill(43, 27, 23);
-    ellipse(windowWidth/2-40, windowHeight/2+60, 25, 75)
-    ellipse(windowWidth/2+40, windowHeight/2+60, 25, 75)
-
-
+    ellipse(windowWidth/2-40, windowHeight/2+60, 25, 75);
+    ellipse(windowWidth/2+40, windowHeight/2+60, 25, 75);
 
 }
+
+*/
+
+function drawPattern() {
+
+  
+
+
+  
+}
+
